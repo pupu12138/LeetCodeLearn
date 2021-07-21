1878f19a741c1989fdc174fd9edf1a6f3fe705b8
pupu12138 <74591944+pupu12138@users.noreply.github.com>
2021-07-21 11:44:04 +0800

    FindZero change

--- src/FindZero.java
+++ src/FindZero.java
@@ -6,8 +6,8 @@ public class FindZero {
     public static void main(String[] args) {
         int[] arr = {0, 1, 0, 2, 3, 12};
         finZero(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        for (int value : arr) {
+            System.out.print(value + " ");
         }
     }
 
