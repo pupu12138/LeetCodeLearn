290b03a28a206f481f113ad9ffbfa24015a6ce00
pupu12138 <74591944+pupu12138@users.noreply.github.com>
2021-07-30 15:31:20 +0800

    TitleToNumber add

---
+++ src/TitleToNumber.java
@@ -0,0 +1,15 @@
+/**
+ * @author WYZ
+ * @creat 2021-07-30 15:28
+ */
+public class TitleToNumber {
+//    未给出方法的主方法
+    public int titleToNumber(String columnTitle) {
+        int res = 0;
+        for(int i = 0;i<columnTitle.length();i++){
+            int num = columnTitle.charAt(i)-'A'+1;
+            res = res*26+num;
+        }
+        return res;
+    }
+}
