16770eb1c32f296a572dc28f07624e8b04f14e25
pupu12138 <74591944+pupu12138@users.noreply.github.com>
2021-08-01 23:45:02 +0800

    IsBalanced add

--- src/IsBalanced.java
+++ src/IsBalanced.java
@@ -2,20 +2,42 @@ import javax.swing.tree.TreeNode;
 
 /**
  * @author WYZ
- * @creat 2021-07-31 1:07
+ * @creat 2021-08-01 23:42
  */
 public class IsBalanced {
-//    public boolean isBalanced(TreeNode root) {
-////        未给出主方法
-//        return dfs(root) != -1;
-//    }
-//
-//    public int dfs(TreeNode root) {
-//        if (root == null) return 0;
-//        int left = dfs(root.left);
-//        if (left == -1) return -1;
-//        int right = dfs(root.right);
-//        if (right == -1) return -1;
-//        return Math.abs(right - left) < 2 ? Math.max(left, right) + 1 : -1;
-//    }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
 }
